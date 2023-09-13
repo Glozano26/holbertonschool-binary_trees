14665a96d538535848483ef92a56fc265feddbb8
Glozano26 <lozsuperbia2014@gmail.com>
2023-09-13 10:26:01 -0700

    size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -12,7 +12,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	
+
 	countleft = binary_tree_size(tree->left);
 	countright = binary_tree_size(tree->right);
 
