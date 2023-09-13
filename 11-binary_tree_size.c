cb043f86b7513c4157dba315486636dbd6e54c0f
Glozano26 <lozsuperbia2014@gmail.com>
2023-09-13 10:24:48 -0700

    size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - function that measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to traverse
+ * Return: measures the height of a binary tree
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t countleft = 0, countright = 0;
+
+	if (tree == NULL)
+		return (0);
+	
+	countleft = binary_tree_size(tree->left);
+	countright = binary_tree_size(tree->right);
+
+	return (countleft + countright + 1);
+
+}
