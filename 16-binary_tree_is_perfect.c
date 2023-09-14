7f7a6cdc33c0d75b11900ea8e39c4590f63c35af
Glozano26 <lozsuperbia2014@gmail.com>
2023-09-14 09:14:26 -0700

    5.c and 7.c tasks

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,75 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_height - goes through a binary tree using pre-order trave
+ * @tree: pointer to the root node of the tree to traverse
+ * Return: measures the height of a binary tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t countleft = 0, countright = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	countleft += binary_tree_height(tree->left);
+	countright += binary_tree_height(tree->right);
+
+	if (countleft > countright)
+		return (countleft + 1);
+	else
+		return (countright + 1);
+
+}
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree:  pointer to the root node balance factor
+ * Return: 0 if tree is NULL
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree:  pointer to the root node of tthe tree check
+ * Return: if tree is NULL, your function must return 0
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
+}
+
+/**
+ * binary_tree_size - function that measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to traverse
+ * Return: measures the height of a binary tree
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t countleft = 0, countright = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	countleft = binary_tree_size(tree->left);
+	countright = binary_tree_size(tree->right);
+
+	return (countleft + countright + 1);
+
+}
 
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
@@ -10,13 +81,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
+	if (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1
+			&& (binary_tree_size(tree->left) - binary_tree_size(tree->right) == 0))
 		return (1);
-	/*comprueba si el nodo y su hijo estan vacios*/
-	if (tree->left == NULL || tree->right == NULL)
+	
+	else
 		return (0);
-
-	return (binary_tree_is_perfect(tree->left)
-			&& binary_tree_is_perfect(tree->right));
-	return (0);
 }
