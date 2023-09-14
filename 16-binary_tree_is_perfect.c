8fb46847bce79e61ce2626b41128812350e7278a
Glozano26 <lozsuperbia2014@gmail.com>
2023-09-13 17:33:23 -0700

    perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -16,6 +16,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree->left == NULL || tree->right == NULL)
 		return (0);
 
-	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
-	/*return (0);*/
+	return (binary_tree_is_perfect(tree->left)
+			&& binary_tree_is_perfect(tree->right));
+	return (0);
 }
