021c0637e5b1cbad6620a3585d39984cc2d28e7d
Glozano26 <lozsuperbia2014@gmail.com>
2023-09-14 14:09:52 -0700

    uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -19,8 +19,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 }
 /**
   *binary_tree_uncle - function that finds the uncle of a node
-  *node: pointer to the node to find the uncle
+  *@node: pointer to the node to find the uncle
   *Return: If node is NULL, return NULL if node if node has no uncle
+  *If node has no uncle, return NULL
   */
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
